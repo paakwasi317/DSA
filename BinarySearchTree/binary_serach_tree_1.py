f48e6a0bdc11665848f51350191851b105b52ef3
paakwasi317 <paakwasi317@gmail.com>
2023-08-17 08:09:45 +0000

    Added some operations to binary search tree

--- BinarySearchTree/binary_serach_tree_1.py
+++ BinarySearchTree/binary_serach_tree_1.py
@@ -19,16 +19,60 @@ class BinarySearchTree:
         if not self.root:
             self.root = Node(data)
         else:
-            self.insert_node(data, parent=self.root)
+            self._insert_node(data, self.root)
 
-    def insert_node(self, data, node: Node):
+    def _insert_node(self, data, node: Node):
         if data < node.data:
             if node.left_node:
-                self.insert_node(data, node.left_node)
+                self._insert_node(data, node.left_node)
             else:
                 node.left_node = Node(data, parent=node)
         else:
-            if not node.right_node:
-                self.insert_node(data, node.right_node)
+            if node.right_node:
+                self._insert_node(data, node.right_node)
             else:
                 node.right_node = Node(data, parent=node)
+
+    def max_item(self):
+        return self._get_max_item(self.root)
+
+    def _get_max_item(self, node: Node):
+        current_node = node
+
+        if current_node.right_node:
+            return self._get_max_item(current_node.right_node)
+        return current_node.data
+
+    def min_item(self):
+        return self._get_min_item(self.root)
+
+    def _get_min_item(self, node: Node):
+        current_node = node
+
+        if current_node.left_node:
+            return self._get_min_item(current_node.left_node)
+        return current_node.data
+
+    def traverse(self):
+        if self.root:
+            self._traverse_in_order(self.root)
+
+    def _traverse_in_order(self, node: Node):
+        if node.left_node:
+            self._traverse_in_order(node.left_node)
+
+        print(node.data)
+        if node.right_node:
+            self._traverse_in_order(node.right_node)
+
+        
+if __name__ == '__main__':
+    binary_search_tree = BinarySearchTree()
+    binary_search_tree.insert(5)
+    binary_search_tree.insert(1)
+    binary_search_tree.insert(6)
+    binary_search_tree.insert(10)
+    binary_search_tree.insert(2)
+    binary_search_tree.insert(15)
+    print(binary_search_tree.min_item())
+    print(binary_search_tree.max_item())
